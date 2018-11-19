1c88fe31e07192ee5f533f2238bdc0bef004a075
AbdoWahba <abdo.tw20154@gmail.com>
2018-11-19 05:32:09 +0200

    the third edition of app

--- reg.py
+++ reg.py
@@ -1,4 +1,30 @@
+import json
 
+cont = 'y'
+while cont == 'y' or cont == 'Y':
+    email = input("Please enter email: ")
 
-f = open("demofile.txt", "a")
-f.write("Now the file has one more line!")
+    while not('@' in email) or not('.'in email) or ("@." in email) \
+            or (email[0] == '@') or (email[-1] == '@') or (email[-1] == '.') \
+            or (' ' in email):
+        email = input("please reenter email correctly: ")
+
+    day = input('Please enter day: ')
+
+    email = email.lower()
+
+    thisdict = {
+        "email": email,
+        "day": day,
+    }
+
+    myJson = json.dumps(thisdict)
+
+    with open("reg.txt", mode='r+') as reg:
+        if email in reg.read():
+            print("sorry, this email has been regestered")
+        else:
+            reg.write(myJson)
+            reg.write(",\n")
+
+    cont = input("do you want to cont?[y/n]")
