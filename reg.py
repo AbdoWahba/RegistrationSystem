5ea9885aca8be8391bea84f989b057a30166f313
Mahmoud Magdy <mahmoudmagdi20@gmail.com>
2018-11-20 18:41:16 +0200

    Add submit button

--- reg.py
+++ reg.py
@@ -4,6 +4,8 @@ from tkinter import ttk
 
 from PIL import Image, ImageTk
 
+import json
+
 
 class Application(Frame):
     def __init__(self, master=None):
@@ -15,6 +17,7 @@ class Application(Frame):
         title_font = font.Font(family="Times", size=24)
         font1 = font.Font(family="Helvetica", size=14)
         style = ttk.Style()
+
         # style.theme_settings("default", {
         #     "TCombobox": {
         #         "configure": {"padding": 5},
@@ -30,8 +33,8 @@ class Application(Frame):
 
         self.frame = ttk.Frame(self)
         self.frame.place(relx=0.5, anchor='n', relwidth=0.3)
-        # self.frame.config(style='frame.TFrame')
 
+        # Header frame
         self.frame_header = ttk.Frame(self.frame)
         self.frame_header.pack(fill=X, expand=1)
 
@@ -42,27 +45,58 @@ class Application(Frame):
         Label(self.frame_header, text="ACES Expo Registration'19",
               bd=0, bg='white', pady=10, font=title_font).pack(fill=X, expand=1)
 
+        # Form frame
         self.frame_form = ttk.Frame(self.frame)
         self.frame_form.pack(fill=X, expand=1)
 
+        # Days label
         ttk.Label(self.frame_form, text="Booth Day:",
                   background='white', font=font1).pack(fill=X, expand=1)
 
-        self.days_value = StringVar()
-        days = ('Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6')
+        # Combo Box
+        self.days_var = IntVar()
+        days = (1, 2, 3, 4, 5, 6)
         days_combo = ttk.Combobox(self.frame_form, values=days,
                                   state="readonly", height=6,
-                                  textvariable=self.days_value)
+                                  textvariable=self.days_var)
         days_combo.pack(fill=X, expand=1)
 
+        # Email label
         ttk.Label(self.frame_form, text="Email:",
                   background='white', font=font1).pack(fill=X, expand=1)
 
-        self.entry_email = ttk.Entry(self.frame_form)
+        # Email entry
+        self.email_var = StringVar()
+        self.entry_email = ttk.Entry(self.frame_form, textvariable=self.email_var)
         self.entry_email.pack(fill=X, expand=1)
+
+        # Submit button
+        ttk.Style().configure("TButton", relief="flat",
+                              background="#00ff00")
+        btn = ttk.Button(self.frame_form, text="Save", command=self.submit)
+        btn.pack()
+
+    # Submit the email to txt file
+    def submit(self):
+        email = str(self.email_var.get())
+        day = self.days_var.get()
+
+        email = email.lower()
+        thisdict = {
+            "email": email,
+            "day": day,
+        }
+        myJson = json.dumps(thisdict)
+
+        with open("reg.txt", mode='r+') as reg:
+            if email in reg.read():
+                print("sorry, this email has been regestered")
+            else:
+                reg.write(myJson)
+                reg.write(",\n")
 
 
 root = Tk()
-root.geometry('640x480+200+200')
+root.geometry('1300x500+200+200')
 app = Application(master=root)
 app.mainloop()
