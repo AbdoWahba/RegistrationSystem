0a2053caf584230a2f959dd163fa7b405e51a6f2
Mahmoud Magdy <mahmoudmagdi20@gmail.com>
2018-11-20 17:54:21 +0200

    Add form without styling and without submit button

--- reg.py
+++ reg.py
@@ -1,5 +1,7 @@
 from tkinter import *
+from tkinter import font
 from tkinter import ttk
+
 from PIL import Image, ImageTk
 
 
@@ -9,15 +11,55 @@ class Application(Frame):
         self.master = master
         self['bg'] = 'white'
         self.pack(expand=True, fill=BOTH)
-        self.create_widgets()
 
-    def create_widgets(self):
+        title_font = font.Font(family="Times", size=24)
+        font1 = font.Font(family="Helvetica", size=14)
+        style = ttk.Style()
+        # style.theme_settings("default", {
+        #     "TCombobox": {
+        #         "configure": {"padding": 5},
+        #         "map": {
+        #             "background": [("active", "green2"),
+        #                            ("!disabled", "green4")],
+        #             "fieldbackground": [("!disabled", "green3")],
+        #             "foreground": [("focus", "OliveDrab1"),
+        #                            ("!disabled", "OliveDrab2")]
+        #         }
+        #     }
+        # })
+
+        self.frame = ttk.Frame(self)
+        self.frame.place(relx=0.5, anchor='n', relwidth=0.3)
+        # self.frame.config(style='frame.TFrame')
+
+        self.frame_header = ttk.Frame(self.frame)
+        self.frame_header.pack(fill=X, expand=1)
+
         self.logo_img = ImageTk.PhotoImage(Image.open("acesLogo.png"))
-        self.logo = Label(self, image=self.logo_img, bd=0)
-        self.logo.place(relx=0.5, y=15, anchor='n')
+        Label(self.frame_header, image=self.logo_img, bd=0,
+              bg='white').pack(fill=X, expand=1)
 
-        self.day_label = ttk.Label(text="Booth Day", background='white')
-        self.day_label.place(relx=0.5, rely=0.5, anchor='center')
+        Label(self.frame_header, text="ACES Expo Registration'19",
+              bd=0, bg='white', pady=10, font=title_font).pack(fill=X, expand=1)
+
+        self.frame_form = ttk.Frame(self.frame)
+        self.frame_form.pack(fill=X, expand=1)
+
+        ttk.Label(self.frame_form, text="Booth Day:",
+                  background='white', font=font1).pack(fill=X, expand=1)
+
+        self.days_value = StringVar()
+        days = ('Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6')
+        days_combo = ttk.Combobox(self.frame_form, values=days,
+                                  state="readonly", height=6,
+                                  textvariable=self.days_value)
+        days_combo.pack(fill=X, expand=1)
+
+        ttk.Label(self.frame_form, text="Email:",
+                  background='white', font=font1).pack(fill=X, expand=1)
+
+        self.entry_email = ttk.Entry(self.frame_form)
+        self.entry_email.pack(fill=X, expand=1)
 
 
 root = Tk()
